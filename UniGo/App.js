8cce05b7fee487fb5862f929fccec18a44ee6286
imannaedmonds <edmondsa20@gmail.com>
2024-02-12 16:04:02 -0800

    updated status sheet v1

--- UniGo/App.js
+++ UniGo/App.js
@@ -1,33 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import Login from './Login';
+import { styles } from './styleSheet'; // 
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.textStyle}> Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Login />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#95A2F1', // App's background color',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  textStyle: {
-    fontFamily: 'Avenir-Black', // Custom font for all text
-  },
-
-  button: {
-    backgroundColor: '#746CC7',
-    padding: 15,
-    borderRadius: 5,
-    width: '80%',
-    alignItems: 'center',
-  },
-
-});
--- UniGo/login.js
+++ UniGo/login.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { styles } from './AppStyles'; // Adjust the path to your styles file
 
-export default function App() {
+export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = () => {
-    // Here you would implement your login logic
     console.log('Login credentials', { email, password });
   };
 
@@ -34,35 +34,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 30,
-    marginBottom: 20,
-  },
-  input: {
-    width: '80%',
-    fontSize: 18,
-    padding: 10,
-    borderBottomWidth: 1,
-    borderColor: 'gray',
-    marginBottom: 20,
-  },
-  button: {
-    backgroundColor: '#746CC7',
-    padding: 15,
-    borderRadius: 5,
-    width: '80%',
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 18,
-  },
-});
