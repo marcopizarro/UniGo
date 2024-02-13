0f87d29edeff3f0545d7e11af8f69ad5bf98b7f8
imannaedmonds <edmondsa20@gmail.com>
2024-02-12 16:05:30 -0800

    added style sheet

---
+++ UniGo/styleSheet.js
@@ -0,0 +1,34 @@
+// AppStyles.js
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#95A2F1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    fontSize: 18,
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: 'gray',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#746CC7',
+    padding: 15,
+    borderRadius: 5,
+    width: '80%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+  },
+});
