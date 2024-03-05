47e73e10c8a619597d13f48ad5b21a40e3898460
marcopizarro <marcoinnovates@gmail.com>
2024-03-04 21:56:51 -0800

    show name

--- UniGo/AreYouOk.js
+++ UniGo/AreYouOk.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { styles } from './StyleSheet'; // Adjust the path to your styles file
 import { Button } from 'react-native';
+import { db } from './firebaseConfig'; // Adjust the path to your firebaseConfig file
+import { addDoc, collection, doc } from 'firebase/firestore';
+import { auth } from './firebaseConfig'; // Adjust the path to your firebaseConfig file
 
 export default function AreYouOk({ route, navigation }) {
 
-  const { pickup, destination } = route.params; 
-  
+  const { pickup, destination } = route.params;
+
   const [selectedButton, setSelectedButton] = useState(null);
 
   // Function to handle when a selection button is pressed
@@ -27,13 +30,28 @@ export default function AreYouOk({ route, navigation }) {
   // Function to handle when the submit button is pressed
   const handleSubmit = () => {
     if (selectedButton !== null) {
+      //send the status to the database
+      try {
+        const docRef = addDoc(collection(db, "rideRequests"), {
+          user: auth.currentUser.uid,
+          pickupLoc: pickup,
+          destinationLoc: destination,
+          rider: selectedButton,
+          time: new Date(),
+          status: "waiting"
+        });
+      }
+      catch (e) {
+        console.error("Error adding document: ", e);
+      }
       // Navigate to the next screen if a button has been selected
-      navigation.navigate('WaitingToBeMatched', { pickup : pickup, destination: destination}); // Replace with your actual next screen name
+      navigation.navigate('WaitingToBeMatched', { pickup: pickup, destination: destination }); // Replace with your actual next screen name
     } else {
       // You can add an alert here if no option is selected
       alert('Please select an option before submitting.');
     }
   };
+
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
